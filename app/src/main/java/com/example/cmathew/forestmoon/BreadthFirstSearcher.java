8c174d4466fe7fff8f3077b78038acd3eee78cc9
cmathew <chris.w.mathew@gmail.com>
2018-01-24 13:51:40 -0800

    detect islands in the graph - those that cannot be reached from the start node

--- app/src/main/java/com/example/cmathew/forestmoon/BreadthFirstSearcher.java
+++ app/src/main/java/com/example/cmathew/forestmoon/BreadthFirstSearcher.java
@@ -1,8 +1,40 @@
 package com.example.cmathew.forestmoon;
 
-/**
- * Created by chris on 1/22/2018.
- */
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BreadthFirstSearcher {
+    private static final String LOG_TAG = "BreadCrumb";
+
+    // Node ID x Node ID matrix
+    // Index of current node
+    // Indexed by Node IDs
+    public void bfs(boolean[][] nodeConnections, int currentNodeIndex, boolean[] visited) {
+        // Cycle detected
+        if (visited[currentNodeIndex]) {
+            return;
+        }
+
+        // Return if all visited
+        boolean allVisited = true;
+        for (int i = 0; i < visited.length; i++) {
+            allVisited = allVisited && visited[i];
+        }
+        if (allVisited) {
+            return;
+        }
+
+        Log.i(LOG_TAG, String.format("Visited node %d", currentNodeIndex + 1));
+        visited[currentNodeIndex] = true;
+
+        //List<Integer> neighborIds = new ArrayList<>();
+        boolean[] currentNodeConnections = nodeConnections[currentNodeIndex];
+        for (int i = 0; i < currentNodeConnections.length; i++) {
+            if (currentNodeConnections[i]) {
+                bfs(nodeConnections, i, visited);
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/cmathew/forestmoon/MainActivity.java
+++ app/src/main/java/com/example/cmathew/forestmoon/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cmathew.forestmoon;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -19,6 +21,9 @@ import javax.xml.*;
 
 public class MainActivity extends AppCompatActivity {
     private boolean[][] connections;
+    private boolean[] seenJamrock;
+
+    private static final String LOG_TAG = "Sailor";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +31,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         try {
-            connections = parseCsv();
+            this.connections = parseCsv();
         } catch (IOException ex) {
             ex.printStackTrace();
+        }
+
+        this.seenJamrock = new boolean[5];
+        for (int i = 0; i < seenJamrock.length; i++) {
+            seenJamrock[i] = false;
+        }
+
+        BreadthFirstSearcher searcher = new BreadthFirstSearcher();
+        searcher.bfs(connections, 0, seenJamrock);
+
+        boolean allVisited = true;
+        for (int i = 0; i < seenJamrock.length; i++) {
+            allVisited = allVisited && seenJamrock[i];
+        }
+
+        TextView outcome = findViewById(R.id.sailor_scan_outcome);
+        if (!allVisited) {
+            outcome.setText("Islands detected.");
+        } else {
+            outcome.setText("Connected. You may travel freely!");
         }
     }
 
@@ -45,18 +70,18 @@ public class MainActivity extends AppCompatActivity {
         Iterable<CSVRecord> csvRecords = csvParser.getRecords();
         for (CSVRecord csvRecord : csvRecords) {
             // Accessing values by Header names
-            int nodeName = Integer.parseInt(csvRecord.get("node_name")) - 1;
+            int nodeId = Integer.parseInt(csvRecord.get("node_name")) - 1;
             boolean connected1 = Integer.parseInt(csvRecord.get("connected_1")) == 1;
             boolean connected2 = Integer.parseInt(csvRecord.get("connected_2")) == 1;
             boolean connected3 = Integer.parseInt(csvRecord.get("connected_3")) == 1;
             boolean connected4 = Integer.parseInt(csvRecord.get("connected_4")) == 1;
             boolean connected5 = Integer.parseInt(csvRecord.get("connected_5")) == 1;
 
-            connections[nodeName][0] = connected1;
-            connections[nodeName][1] = connected2;
-            connections[nodeName][2] = connected3;
-            connections[nodeName][3] = connected4;
-            connections[nodeName][4] = connected5;
+            connections[nodeId][0] = connected1;
+            connections[nodeId][1] = connected2;
+            connections[nodeId][2] = connected3;
+            connections[nodeId][3] = connected4;
+            connections[nodeId][4] = connected5;
         }
 
         return connections;
